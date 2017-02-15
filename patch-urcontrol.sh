42c15ecb195bef2aa61e2759aa853310d65e6d54
Hans Gaiser <j.c.gaiser@delftrobotics.com>
2017-02-15 13:16:33 +0100

    Add check for patchelf existence.

--- patch-urcontrol.sh
+++ patch-urcontrol.sh
@@ -5,6 +5,12 @@ log_exec() {
 	"$@"
 }
 
+# test for patchelf existence
+if ! type patchelf > /dev/null 2>&1; then
+	echo >&2 "Patching requires the patchelf tool, please install it."
+	exit 1
+fi
+
 if ! log_exec patchelf --set-rpath "$PWD/lib" ./URControl; then
 	echo "Failed to set ./URControl rpath. Please fix manually."
 fi
